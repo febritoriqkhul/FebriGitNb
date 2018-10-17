1e826c0ca363392cce2ec6158e9131792510941a
Febri TK <Febri TK@172.16.182.242>
2018-10-17 07:42:51 +0700

    Init project 1

---
+++ src/TestGit/Git1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestGit;
+
+/**
+ *
+ * @author Febri TK
+ */
+public class Git1 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+    
+        System.out.print(x);
+    }
+}
